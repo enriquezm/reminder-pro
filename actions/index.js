41e81cd35bc4094211a64e6c9a46d04d04fbf46f
Myles Enriquez <enri.myles@gmail.com>
2018-04-17 23:01:39 -0700

    Set up action.

---
+++ actions/index.js
@@ -0,0 +1,11 @@
+export { ADD_REMINDER } from '../constants';
+
+export const addReminder = (text) => {
+  const action = {
+    type: ADD_REMINDER,
+    text
+  }
+
+  console.log('action in addReminder', action);
+  return action;
+}
